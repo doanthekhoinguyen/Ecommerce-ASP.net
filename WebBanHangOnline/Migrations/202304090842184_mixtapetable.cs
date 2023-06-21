32a25fa7cd1ef037bf2dfc5bd370b6db1cf1d464
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-21 14:48:00 +0700

    Add project files.

---
+++ WebBanHangOnline/Migrations/202304090842184_mixtapetable.cs
@@ -0,0 +1,30 @@
+﻿namespace WebBanHangOnline.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mixtapetable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Mixtapes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Color = c.Int(nullable: false),
+                        LabelStyle = c.Int(nullable: false),
+                        LinkA = c.String(),
+                        LinkB = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Mixtapes");
+        }
+    }
+}
---
+++ WebBanHangOnline/Models/EF/Mixtape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHangOnline.Models.EF
+{
+    public class Mixtape
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Color { get; set; }
+        public int LabelStyle { get; set; }
+        public string LinkA { get; set; }
+        public string LinkB { get; set; }
+    }
+}
